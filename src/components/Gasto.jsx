93d20772fe062a47a5bcb0042aa203bb3e046c0a
Abraham Espinosa <abrahamespinosa3132@gmail.com>
2022-02-23 01:10:17 -0600

    Swipe actions

--- src/components/Gasto.jsx
+++ src/components/Gasto.jsx
@@ -1,3 +1,12 @@
+import {
+	LeadingActions,
+	SwipeableList,
+	SwipeableListItem,
+	SwipeAction,
+	TrailingActions,
+} from "react-swipeable-list";
+import "react-swipeable-list/dist/styles.css";
+
 import IconoAhorro from "../img/icono_ahorro.svg";
 import IconoCasa from "../img/icono_casa.svg";
 import IconoComida from "../img/icono_comida.svg";
@@ -19,19 +28,34 @@ const diccionarioIconos = {
 export const Gasto = ({ datosDelGasto }) => {
 	const { nombre, gasto, categoria, fecha } = datosDelGasto;
 
+	const leadingActions = () => {
+
+	}
+
+	const trailingActions = () => {
+		
+	}
+
 	return (
-		<div className="gasto sombra">
-			<div className="contenido-gasto">
-				<img src={diccionarioIconos[categoria]} alt={categoria} />
-				<div className="descripcion-gasto">
-					<p className="categoria">{categoria}</p>
-					<p className="nombre-gasto">{nombre}</p>
-					<p className="fecha-gasto">
-						Agregado el: <span>{fecha}</span>
-					</p>
+		<SwipeableList>
+			<SwipeableListItem
+				leadingActions={leadingActions} // Acciones de izquierda a derecha
+				trailingActions={trailingActions} // De derecha a izquierda
+			>
+				<div className="gasto sombra">
+					<div className="contenido-gasto">
+						<img src={diccionarioIconos[categoria]} alt={categoria} />
+						<div className="descripcion-gasto">
+							<p className="categoria">{categoria}</p>
+							<p className="nombre-gasto">{nombre}</p>
+							<p className="fecha-gasto">
+								Agregado el: <span>{fecha}</span>
+							</p>
+						</div>
+					</div>
+					<p className="cantidad-gasto">${gasto}</p>
 				</div>
-			</div>
-			<p className="cantidad-gasto">${gasto}</p>
-		</div>
+			</SwipeableListItem>
+		</SwipeableList>
 	);
 };
